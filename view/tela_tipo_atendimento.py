6e3def17c39e4ff7d9fce336426c4cecbeff0149
wellonlywell <well.apenaswell@gmail.com>
2026-06-05 03:01:53 -0300

    atualiza telas tipo atendimento e procedimento

--- view/tela_tipo_atendimento.py
+++ view/tela_tipo_atendimento.py
@@ -1,8 +1,7 @@
+from exceptions.dado_invalido_exception import DadoInvalidoException
 
 class TelaTipoAtendimento:
-  # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def tela_opcoes(self):
-    """Mostra o menu de opções para Tipos de Atendimento."""
     print("-------- TIPOS DE ATENDIMENTO ----------")
     print("1 - Incluir Tipo de Atendimento")
     print("2 - Alterar Tipo de Atendimento")
@@ -16,33 +15,32 @@ class TelaTipoAtendimento:
         opcao = int(input("Escolha a opção: "))
         if opcao in [0, 1, 2, 3, 4]:
           return opcao
-        print("Opção inválida! Digite um número entre 0 e 4.")
+        raise DadoInvalidoException("Opção inválida! Digite um número entre 0 e 4.")
       except ValueError:
-        print("Por favor, digite um número inteiro válido.")
+        raise DadoInvalidoException("Digite um número inteiro válido.")
+      except DadoInvalidoException as e:
+        print(e)
 
   def pega_dados_tipo_atendimento(self):
-    """Pede os dados para cadastrar ou alterar um tipo de atendimento."""
     print("\n----- INSERIR DADOS DO TIPO DE ATENDIMENTO -----")
-    codigo = input("Código identificador (Ex: 101, RE, AMB): ").strip().upper()
-    nome = input("Nome do Tipo de Atendimento (Ex: Consulta Eletiva, Retorno): ").strip()
-
-    return {
-      "codigo": codigo,
-      "nome": nome
-    }
+    descricao = input("Descrição (Ex: Consulta Eletiva, Retorno): ").strip()
+    return {"descricao": descricao}
 
   def mostra_tipo_atendimento(self, tipo_atendimento):
-    """Exibe os dados de um tipo de atendimento cadastrado."""
-    print(f"Código: {tipo_atendimento.codigo}")
-    print(f"Nome: {tipo_atendimento.nome}")
+    print(f"ID: {tipo_atendimento.id}")
+    print(f"Descrição: {tipo_atendimento.descricao}")
     print("-" * 40)
 
-  def seleciona_tipo_atendimento(self) -> str:
-    """Pede o código para encontrar um tipo de atendimento específico."""
+  def seleciona_tipo_atendimento(self) -> int:
     print("\n----- SELECIONAR TIPO DE ATENDIMENTO -----")
-    codigo = input("Digite o CÓDIGO do tipo de atendimento: ").strip().upper()
-    return codigo
+    while True:
+      try:
+        id = int(input("Digite o ID do tipo de atendimento: ").strip())
+        return id
+      except ValueError:
+        raise DadoInvalidoException("Digite um número inteiro válido.")
+      except DadoInvalidoException as e:
+        print(e)
 
   def mostra_mensagem(self, mensagem: str):
-    """Mostra qualquer mensagem de sucesso ou erro no terminal."""
     print(f"\n[Aviso]: {mensagem}")
