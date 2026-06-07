cb2d2fa1dc4d6cf3e12febf007e4d5fea34be416
wellonlywell <well.apenaswell@gmail.com>
2026-06-07 20:59:26 -0300

    remove controladores de pagamento individuais nao utilizados

--- control/controlador_pag_dinheiro.py
+++
@@ -1,7 +0,0 @@
-class ControladorPagDinheiro():
-
-  def __init__(self, controlador_sistema):
-    self.__pag_dinheiro = []
-    self.__tela_pag_dinheiro = TelaPagDinheiro()
-    self.__controlador_sistema = controlador_sistema
-pass
--- control/controlador_pag_pix.py
+++
@@ -1,7 +0,0 @@
-class ControladorPagamentoPix():
-
-  def __init__(self, controlador_sistema):
-    self.__pag_pix = []
-    self.__tela_pag_pix = TelaPagPix()
-    self.__controlador_sistema = controlador_sistema   
-pass
